807f4491c37746e49aacb9ad269303bcbad08637
Holly Vaccarella <hollyvacc@gmail.com>
2017-10-07 17:28:41 -0400

    Put ingredients in a dictionary based on input.

--- recipe.py
+++ recipe.py
@@ -1,15 +1,24 @@
 from database import *
+from collections import defaultdict
 #recipes - name of database
 
 def main():
     ingredient = input('Enter an ingredient: ')
-
-    for k, v in recipes.items():
+    ingreCount = {}
+    ingreCount = defaultdict(lambda: 0, ingreCount)
+    
+    for recipe, ingreList in recipes.items():
         #for ingre in enumerate(v):
-        for ingre in v:    
-            if ingre == "Spinach":
-                print(ingre)
+        #if ingredient is in ingreList then traverse the ingreList 
+        #and if the ingre != ingredient then add to ingreCount        
+        if ingredient in ingreList:
+            for ingre in ingreList:    
+                #add ingre to map if not ingredient & increment counter
+                    if ingre != ingredient:
+                        ingreCount[ingre] += 1
 
+    print(ingreCount)
     return;
+
 
 main()
